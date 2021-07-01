37fe28a7e024249a0961104d779543094f7c8642
proivited <miguel.alexis.gomez.a@gmail.com>
2021-07-01 16:54:10 -0500

    Son los operadores logicos del 1 de julio

---
+++ OperadoresLogicos/src/OperadoresLogicos.java
@@ -0,0 +1,28 @@
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Operadores logicos 
+		 * a == b a es igual a b
+		 * a > b a es mayor que b
+		 * a < b a es menor qeu b
+		 * a != b a es diferente a b
+		 * >= <= mayor igual menor igal
+		 */
+		// System.out.println(saludo());
+		saludo();
+		
+
+	}
+	
+	//publico estatic
+	//public static String saludo() {
+	public static void saludo() {
+		System.out.println("saludo1");
+		System.out.println("saludo2");
+		//return "saludo";
+	}
+
+}
